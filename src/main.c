76ec65f61f393485b1d5ea11d50d59ca9b1bccff
Cai-linhai <linhai_cai@163.com>
2022-05-21 14:10:59 +0800

    main

--- src/main.c
+++ src/main.c
@@ -7,11 +7,12 @@ int main()
     sds name = sdsnew("cailinhai");
     sdshdr *sh = (void *)(name - (sizeof(sdshdr)));
 
+    printf("name is %s.\n", name);
+    printf("name len is %d, avail is %d.\n", sh->len, sh->avail);
+
     sds newname = sdscat(name, " is so handsam!");
     sdshdr *newsh = (void *)(newname - (sizeof(sdshdr)));
     
-    printf("name is %s.\n", name);
-    printf("name len is %d, avail is %d.\n", sh->len, sh->avail);
     printf("newname is %s.\n", newname);
     printf("newname len is %d, avail is %d.\n", newsh->len, newsh->avail);
 
