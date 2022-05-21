754b3d1ecbb553e2fa3ef6cb74cc711db156e905
Cai-linhai <linhai_cai@163.com>
2022-05-21 20:10:40 +0800

    sds-api

--- src/main.c
+++ src/main.c
@@ -5,16 +5,14 @@
 int main()
 {
     sds name1 = sdsnew("cailinhai");
-    sdshdr *sh = (void *)(name1 - (sizeof(sdshdr)));
     sds name2 = sdsnew("cailinhai");
-    sdshdr *sh = (void *)(name2 - (sizeof(sdshdr)));
 
     int cmp = sdscmp(name1, name2);
     printf("name1 = name2 is %s.\n", cmp ? "false" : "true");
 
-
     sds newname = sdscpy(name1, "Cailinhai is so handsam!");
     sdshdr *newsh = (void *)(newname - (sizeof(sdshdr)));
+    
     printf("newname copy is %s.\n", newname);
     printf("newsh->len is %d, newsh->avail is %d.\n", newsh->len, newsh->avail);
 
